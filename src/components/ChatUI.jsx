6bb033417e9b3b3295b30f19cb37694dc7e8ed91
Danny Niedzwiedzki <danielniedzwiedzki.1@gmail.com>
2024-07-31 22:24:33 -0400

    update to chatbot

--- src/components/ChatUI.jsx
+++ src/components/ChatUI.jsx
@@ -4,6 +4,9 @@ import { FaRobot } from 'react-icons/fa';
 import { LuMinimize2 } from 'react-icons/lu';
 import { BsFillPersonFill } from 'react-icons/bs';
 import { customFetch } from '../utils/chatbotMsgFetch';
+import { FiGithub } from 'react-icons/fi';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'; // optional
 
 const ChatUI = ({ setIsChatVisible, transcript, setTranscript }) => {
   const [text, setText] = useState('');
@@ -32,6 +35,7 @@ const ChatUI = ({ setIsChatVisible, transcript, setTranscript }) => {
       ]);
 
       setIsTyping(true);
+      // Make API call to FastAPI
       try {
         const { data } = await customFetch.post('/chat', {
           question: text,
@@ -69,21 +73,46 @@ const ChatUI = ({ setIsChatVisible, transcript, setTranscript }) => {
   };
 
   return (
-    <div className="flex flex-col h-screen md:h-[30rem] w-full md:w-[20rem] rounded-lg shadow-lg">
+    <div className="h-screen md:h-[30rem] md:w-[20rem] flex flex-col rounded-lg shadow-lg">
       <div className="flex justify-between items-center bg-gray-500 text-accent-content p-3 rounded-t-lg">
-        <IoCloseSharp
-          className="text-3xl cursor-pointer text-gray-100 hover:scale-[1.05] duration-100"
-          onClick={closeChat}
-        />
-        <LuMinimize2
-          className="text-2xl text-gray-100 cursor-pointer hover:scale-[1.05] duration-100"
-          onClick={() => setIsChatVisible(false)}
-        />
+        <div className="relative">
+          <Tippy
+            animation={'fade'}
+            placement={'right'}
+            duration={300}
+            content={
+              <p className="text-xs italic">
+                Click here to check <br /> out the source code
+              </p>
+            }
+          >
+            <div>
+              <a
+                href="https://github.com/ndnied91/Chatbot-GPT"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FiGithub className="text-3xl cursor-pointer text-error hover:scale-[1.05] duration-100" />
+              </a>
+            </div>
+          </Tippy>
+        </div>
+        <div className="flex items-center gap-2">
+          <IoCloseSharp
+            className="text-3xl cursor-pointer text-gray-100 hover:scale-[1.05] duration-100"
+            onClick={closeChat}
+          />
+          <LuMinimize2
+            className="text-2xl text-gray-100 cursor-pointer hover:scale-[1.05] duration-100"
+            onClick={() => setIsChatVisible(false)}
+          />
+        </div>
       </div>
-      <div className="flex flex-col flex-grow bg-slate-100 relative">
+
+      <div className="bg-slate-100 flex flex-grow flex-col h-max rounded-b-lg pb-18 md:mb-0">
         <div
           ref={chatContainerRef}
-          className="flex-grow px-4 md:px-2 py-2 overflow-y-auto text-gray-800 mb-14"
+          className="pl-4 pr-4 md:pl-2 md:pr-2 flex-grow text-gray-800 h-0 md:max-h-[24rem] overflow-scroll mb-2"
         >
           {transcript.map(({ user, text }, index) => {
             if (index === 0) {
@@ -94,15 +123,6 @@ const ChatUI = ({ setIsChatVisible, transcript, setTranscript }) => {
                   </div>
                   <div className="italic text-lg md:text-xs mt-2 text-red-800">
                     {text}
-                    <span className="pl-1 italic text-gray-500">
-                      <a
-                        href="https://github.com/ndnied91/Chatbot-GPT"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Check out the Github repo here
-                      </a>
-                    </span>
                   </div>
                 </div>
               );
@@ -110,11 +130,11 @@ const ChatUI = ({ setIsChatVisible, transcript, setTranscript }) => {
             return (
               <div key={index}>
                 <div
-                  className={`${
+                  className={` ${
                     user === 'human'
                       ? 'flex items-center gap-2 mt-3 text-right text-gray-900 flex-row-reverse'
-                      : 'flex items-center gap-2 mt-3 text-gray-500 p-2 rounded-md'
-                  }  `}
+                      : 'flex items-center gap-2 mt-3 text-gray-600 p-2 rounded-md'
+                  } `}
                 >
                   {user === 'human' ? (
                     <BsFillPersonFill className="text-xl" />
@@ -127,8 +147,9 @@ const ChatUI = ({ setIsChatVisible, transcript, setTranscript }) => {
             );
           })}
         </div>
-        <div className="absolute bottom-0 left-0 w-full flex border-t-2 border-gray-300 bg-slate-100">
-          <div className="absolute bottom-14 md:bottom-12 italic text-gray-700 pl-2 pb-0.5 md:text-sm">
+
+        <div className="w-full flex border-t-2 border-gray-300 rounded-b-md bg-slate-100">
+          <div className="absolute bottom-14 md:bottom-12 rounded-br-lg italic text-gray-400 pl-2 pb-0.5 md:text-sm">
             {isTyping && <p>Virtual assistant is typing..</p>}
           </div>
           <div className="flex w-full">
@@ -136,7 +157,7 @@ const ChatUI = ({ setIsChatVisible, transcript, setTranscript }) => {
               value={text}
               onChange={handleInputChange}
               onKeyPress={handleKeyPress}
-              className="w-full bg-slate-100 h-14 md:h-12 font-semibold pl-3 text-md md:text-xs focus:outline-none text-gray-900"
+              className="w-full bg-slate-100 h-14 md:h-12 font-semibold pl-3 flex-1 text-md md:text-xs rounded-b-lg focus:outline-none text-gray-900"
               placeholder="Type your question here..."
             />
             <button
